c144b95bc2b78cefd47a2c2267eb648a43176f9c
CoNe13132! <cjm308@pitt.edu>
2025-02-15 20:45:51 -0500

    Added combo box functionality

--- module-wayside-controller/wayside-controller-QT/wayside_controller_testbench.py
+++ module-wayside-controller/wayside-controller-QT/wayside_controller_testbench.py
@@ -1,7 +1,7 @@
 """Connor Murray breh"""
 import sys
-from PyQt5.QtWidgets import QApplication, QMainWindow
-from PyQt5.QtCore import pyqtSignal
+from PyQt5.QtWidgets import QApplication, QMainWindow, QListWidgetItem
+from PyQt5.QtCore import pyqtSignal, pyqtSlot
 from wayside_controller_testbench_ui import Ui_MainWindow
 
 """Describe the class"""
@@ -32,10 +32,23 @@ class WaysideTestbenchApp(QMainWindow):
         self.ui.select_block_list.itemClicked.connect(self.handle_block_selection)
         self.ui.suggested_speed_confirm_button.clicked.connect(self.handle_speed_confirmation)
         self.ui.suggested_authority_confirm_button.clicked.connect(self.handle_authority_confirmation)
-        
+        self.ui.block_occupancy_confirm_button.clicked.connect(self.handle_occupancy_confirmation)
+
+    @pyqtSlot(QListWidgetItem)  
     def handle_block_selection(self, selected_block):
-        self.current_block_index = self.ui.select_block_list.row(selected_block) + 1 # Updating the index based on the block selected (use 1 indexing)
+        self.current_block_index = self.ui.select_block_list.row(selected_block) # Updating the index based on the block selected
         
+        # Update the gui so that it reflects what the user has input previously, or if yet to input anything set to defaults
+        # Update combo box for block occupancy 
+        if(self.block_occupancies[self.current_block_index] == "Unoccupied"):
+            self.ui.block_occupancy_combo_box.setCurrentIndex(0) # match combo box to unoccupied if never accessed before or has been set to Unoccupied
+        elif(self.block_occupancies[self.current_block_index] == "Occupied"):
+            self.ui.block_occupancy_combo_box.setCurrentIndex(1) # match combo box to the user input of occupied
+        elif(self.block_occupancies[self.current_block_index] == "Maintenance"):
+            self.ui.block_occupancy_combo_box.setCurrentIndex(2) # match combo box to the user input of maintenance
+        elif(self.block_occupancies[self.current_block_index] == "Track Failure"):
+            self.ui.block_occupancy_combo_box.setCurrentIndex(3) # match combo box to the user input of track failure
+
         # Update the suggested speed fields to match the corresponding blocks user input, otherwise clear the block since it has nothing 
         if self.suggested_speeds[self.current_block_index] == "Not Set":
             self.ui.suggested_speed_line_edit.clear()
@@ -49,16 +62,28 @@ class WaysideTestbenchApp(QMainWindow):
             self.ui.suggested_authority_line_edit.clear()
         else:
             self.ui.suggested_authority_line_edit.setText(self.suggested_authorities[self.current_block_index])
-
+    
+    @pyqtSlot()
     def handle_speed_confirmation(self):
-        # When the confirm button is clicked update the speed and emit a signal
-        self.suggested_speeds[self.current_block_index] = self.ui.suggested_speed_line_edit.text()
-        self.suggested_speed.emit(self.current_block_index, self.ui.suggested_speed_line_edit.text())
-         
+        if self.current_block_index is not None: # Making sure the confirmation button only updates when a block is selected
+            # When the confirm button is clicked update the speed and emit a signal
+            self.suggested_speeds[self.current_block_index] = self.ui.suggested_speed_line_edit.text()
+            self.suggested_speed.emit(self.current_block_index, self.ui.suggested_speed_line_edit.text())
+    
+    @pyqtSlot()  
     def handle_authority_confirmation(self):
-        # When the confirm button is clicked update the authority and emit a signal
-        self.suggested_authorities[self.current_block_index] = self.ui.suggested_authority_line_edit.text()
-        self.suggested_authority.emit(self.current_block_index, self.ui.suggested_authority_line_edit.text())
+        if self.current_block_index is not None: # Making sure the confirmation button only updates when a block is selected
+            # When the confirm button is clicked update the authority and emit a signal
+            self.suggested_authorities[self.current_block_index] = self.ui.suggested_authority_line_edit.text()
+            self.suggested_authority.emit(self.current_block_index, self.ui.suggested_authority_line_edit.text())
+    
+    @pyqtSlot()
+    def handle_occupancy_confirmation(self):
+        if self.current_block_index is not None: # Making sure the confirmation button only updates when a block is selected
+            # When the confirm button is clicked update the occupancy in accordance with the current state of the combo box
+            self.block_occupancies[self.current_block_index] = self.ui.block_occupancy_combo_box.currentText()
+            self.block_occupancy.emit(self.current_block_index, self.ui.block_occupancy_combo_box.currentText())
+
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     main_window = WaysideTestbenchApp()
