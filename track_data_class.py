8b83c12d091b32dd9ff645b51781eebb607cf46b
Connor Murray <cjm308@pitt.edu>
2025-03-30 16:29:46 -0400

    Added pandas to the .yml and created track data class definition

---
+++ track_data_class.py
@@ -0,0 +1,11 @@
+
+import pandas as pd
+
+class TrackDataClass():
+    def __init__(self, filepath: str):
+        """
+        Initialization for the static track data.
+
+        :param filepath: The filepath to an excel containing information about the track
+        """
+
