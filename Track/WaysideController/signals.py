65d04ba40ccd8a6b8338c934fb404982ddf46148
Connor Murray <cjm308@pitt.edu>
2025-03-30 16:19:14 -0400

    Defined qt signals

--- Track/WaysideController/signals.py
+++ Track/WaysideController/signals.py
@@ -10,3 +10,16 @@ from PyQt5.QtCore import pyqtSignal
 
 
 
+ctc_switch_states = pyqtSignal(list[bool])
+ctc_exit_blocks = pyqtSignal(list[bool],list[bool],list[bool])
+ctc_dispatch = pyqtSignal()
+ctc_maintenance = pyqtSignal(int, bool)
+ctc_suggested = pyqtSignal(list[float],list[float])
+
+
+wayside_block_occupancies = pyqtSignal(list[bool])
+wayside_switches = pyqtSignal(list[bool])
+wayside_lights = pyqtSignal(list[bool])
+wayside_crossings = pyqtSignal(list[bool])
+
+track_tickets = pyqtSignal(int)
