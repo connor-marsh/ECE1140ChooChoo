3e955a768520783cdbcbbee5140c1dbccf1f8aeb
Connor Murray <cjm308@pitt.edu>
2025-03-30 23:45:39 -0400

    modified excel

--- src/globals/track_data_class.py
+++ src/globals/track_data_class.py
@@ -9,17 +9,31 @@ class TrackDataClass():
         :param filepath: The filepath to an excel containing information about the track
         """
         file_data = pd.read_excel(filepath)
-        self.blocks = self.read_output(file_data)
+        
+        self.blocks, self.switches, 
+        self.lights, self.crossings,
+        self.stations, self.beacons = self.parse_output(file_data)
 
-    def read_output(self, file_data: dict):
+
+    def parse_output(self, file_data: dict):
         """
         Takes the output pandas dictionary from the excel file and converts it to a more usable form
 
         :param file_data: A dictionary containing the output from the track definition excel sheet
 
-        :return :
+        :return blocks:
+        
+        :return switches:
 
+        :return lights:
+
+        :return crossings:
+
+        :return stations:
+        
+        :return beacons:
         """
 
         
-        
+
+    
