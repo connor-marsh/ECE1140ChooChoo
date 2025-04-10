6b19d46930208bbe922b32995fcb3b58d6fe7352
Aragya Goyal <agoyal1642@gmail.com>
2025-04-10 17:20:56 -0400

    Fix drift and add UI window names

--- src/Train/TrainController/train_controller_ui.py
+++ src/Train/TrainController/train_controller_ui.py
@@ -564,7 +564,7 @@ class Ui_MainWindow(object):
 
     def retranslateUi(self, MainWindow):
         _translate = QtCore.QCoreApplication.translate
-        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        MainWindow.setWindowTitle(_translate("MainWindow", "Train Controller"))
         self.groupBox_6.setTitle(_translate("MainWindow", "Control Constants"))
         self.ki_line_edit.setPlaceholderText(_translate("MainWindow", "Default = 75"))
         self.label_23.setText(_translate("MainWindow", "Kp Value"))
